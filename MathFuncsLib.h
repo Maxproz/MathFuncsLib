9f29a33178f220a4fc2a6ed25a53d6dbb7396828
Maxpro-PC\Maxpro <maxdietz@hotmail.com>
2018-05-24 13:16:30 -0700

    Added some functions for using common conversions

--- MathFuncsLib.h
+++ MathFuncsLib.h
@@ -27,6 +27,64 @@ namespace MathFuncs
 
 	};
 
+	namespace Conversions
+	{
+		// Weight conversions
+		// pounds to kilograms
+		constexpr long double operator"" _pounds(long double pounds)
+		{
+			return pounds / 2.2046;
+		}
+
+		// kilograms to pounds
+		constexpr long double operator"" _kilograms(long double kg)
+		{
+			return kg * 2.2046;
+		}
+
+
+
+
+		// Distance conversions
+		constexpr long double operator"" _m(long double m)
+		{
+			// returns feet
+			return m * 3.2808;
+		}
+
+		// fluid ounces to milliliters 
+		constexpr long double operator"" _ft(long double ft)
+		{
+			// returns meters
+			return ft / 3.2808;
+		}
+
+
+
+
+
+		// Volume conversions
+		// milliliters to fluid ounces 
+		constexpr long double operator"" _mL(long double mL)
+		{
+			// returns us fl oz
+			return mL * 0.033814;
+		}
+
+		// fluid ounces to milliliters 
+		constexpr long double operator"" _floz(long double floz)
+		{
+			// returns milliliters
+			return floz / 0.033814;
+		}
+
+
+	}
+
+
+
+
+
 	// NOTE: static librarys don't only use static functions..
 	// The static is implying it changes when compiled with executible?
 
